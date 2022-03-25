24870f1d1e8398630b5030a1c8db97f20d310352
myOmikron <git@omikron.dev>
2022-03-25 23:23:25 +0100

    Added types for determine the spawn of a species

--- models/pokemon.go
+++ models/pokemon.go
@@ -1,5 +1,45 @@
 package models
 
+type WeatherType struct {
+	ID uint `gorm:"primarykey" json:"id"`
+}
+
+type MoonType struct {
+	ID uint `gorm:"primarykey" json:"id"`
+}
+
+type TimeType struct {
+	ID uint `gorm:"primarykey" json:"id"`
+}
+
+type SpawnArea struct {
+	ID uint `gorm:"primarykey" json:"id"`
+}
+
+type Modifier struct {
+	ID       uint `gorm:"primarykey"`
+	Modifier float64
+}
+
+type Condition struct {
+	ID           uint `gorm:"primarykey"`
+	Index        uint `gorm:"default:1"` // Greater is better xD
+	ModifierID   uint
+	Modifier     Modifier
+	WeatherTypes []WeatherType
+	MoonTypes    []MoonType
+	TimeTypes    []TimeType
+}
+
+type PokemonSpawnRelation struct {
+	PokemonID   uint
+	Pokemon     Pokemon
+	SpawnAreaID uint
+	SpawnArea   SpawnArea
+	Probability float64
+	Conditions  []Condition
+}
+
 type Pokemon struct {
 	ID uint `gorm:"primarykey" json:"id"`
 }
