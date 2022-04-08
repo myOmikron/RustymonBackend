4fd29c3586b2c65c12f73e9d8ad1d26988d7a4a0
myOmikron <git@omikron.dev>
2022-04-08 03:58:54 +0200

    Added reference to Pokemon in PokedexEntry

--- models/pokedex.go
+++ models/pokedex.go
@@ -4,8 +4,11 @@ import "github.com/myOmikron/echotools/utilitymodels"
 
 type PokedexEntry struct {
 	utilitymodels.CommonSoftDelete
-	CaughtCount    uint16 `json:"caught_count"`
-	SeenCount      uint16 `json:"seen_count"`
-	ShinySeenCount uint16 `json:"shiny_seen_count"` // ShinySeenCount is included in SeenCount
-	PlayerID       uint   `json:"player_id"`
+	PokemonID        uint    `json:"pokemon_id"`
+	Pokemon          Pokemon `json:"-"`
+	CaughtCount      uint16  `json:"caught_count"`
+	SeenCount        uint16  `json:"seen_count"`
+	ShinySeenCount   uint16  `json:"shiny_seen_count"`   // ShinySeenCount is included in SeenCount
+	ShinyCaughtCount uint16  `json:"shiny_caught_count"` //ShinyCaughtCount is included in CaughtCount
+	PlayerID         uint    `json:"player_id"`
 }
